6f6609a31b0bc99b0af4003553c9502f5dab38b2
Gaspard Lacave <gl152415@edu.ece.fr>
2018-04-17 15:43:22 +0200

    Ajout de la liste  champs table et de la string nom_table pour les methodes de recherche/ajout/supression

--- projet_hopital/src/modele/gestionBDD.java
+++ projet_hopital/src/modele/gestionBDD.java
@@ -16,17 +16,46 @@ import java.util.ArrayList;
 
 public class gestionBDD {
     private Connexion local;
-    private ArrayList<Object> table;
+    private ArrayList<Object> table;//données de la table
+    private ArrayList<Object> champs_table;//nom des propriétes de la table
+    private String nom_table;//nom de la table recherchée obetnue dnas le menu de recherche
     
     public gestionBDD() throws ClassNotFoundException, SQLException{
         local = new Connexion("projet_java","root","");
-       
+        
+        ///test pour bosser sur un exemple (docteur ici)
+        champs_table = local.remplirChampsTable("docteur");
+        for(Object elem : champs_table){
+        System.out.println(elem+"  ");
+        }
+
+        table = local.remplirChampsRequete("SELECT * FROM docteur");
+        for(Object elem : table){
+        System.out.println(elem+"  ");
+        }
+        nom_table="docteur";
+        ///fin du test
+
     }
     
     
-    public void remove(int numero_objet)
+    public void remove(int numero_objet) throws SQLException
     {
+        ///test
+        String request="DELETE FROM ";
+        request+=nom_table;
+        //convertir object en string
+        // extraire id donc tout avant la virgule
+        //
+        /*table[]=4;
         
+        request+="WHERE condition";
+        
+        local.ajouterRequeteMaj(request);
+        local.executeUpdate(request);
+        table.remove(numero_objet);*/
+        //réafficher la liste table actualisé sans l'élément suprrimé
+
     }
     
     public void add()
